3d34a721c249d957e32baef2dd687ccee980845c
MALSH100 <maj.alshibly@gmail.com>
2026-05-16 22:51:36 +0100

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -3,6 +3,7 @@
 // New: Item drops, Officer war start, Paginated /clan-commands,
 //      /pokemon-view, /pokemon-stats, channel rename fix, stale channel fix
 // ═══════════════════════════════════════════════════════════════════════════════
+console.log("NEW VERSION LOADED 12345");
 
 const {
   SlashCommandBuilder,
